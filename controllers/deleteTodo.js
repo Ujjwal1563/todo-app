ceed1d7d29438d539619061e3309193084107795
Ujjwal Sharma <101029502+Ujjwal1563@users.noreply.github.com>
2024-07-09 15:31:15 +0530

    new

---
+++ controllers/deleteTodo.js
@@ -0,0 +1,23 @@
+const Todo = require("../models/todo");
+
+exports.deleteTodo = async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        await Todo.findByIdAndDelete(id);
+        res.json({
+            success:true,
+            message:"Todo Deleted",
+        })
+    }
+    catch (err) {
+        console.error(err);
+        console.log(err);
+        res.status(500)
+        .json({
+            success:false,
+            data:"internal server error",
+            message: err.message,
+        }); 
+    }
+}
